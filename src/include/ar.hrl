e12c47a24a09dd251e8ec1fe49968da6c402d8a3
James Piechota <piechota@gmail.com>
2025-05-19 19:31:07 +0000

    feat: bring over ar_tx and ar_merkle from Arweave

--- src/include/ar.hrl
+++ src/include/ar.hrl
@@ -1,3 +1,11 @@
+%% Maximum size of a single data chunk, in bytes.
+-define(DATA_CHUNK_SIZE, (256 * 1024)).
+
+%% The size of data chunk hashes, in bytes.
+-define(CHUNK_ID_HASH_SIZE, 32).
+
+-define(NOTE_SIZE, 32).
+
 -define(DEFAULT_SIG, << 0:4096 >>).
 -define(DEFAULT_ID, << 0:256 >>).
 -define(DEFAULT_OWNER, << 0:4096 >>).
@@ -5,6 +13,28 @@
 
 -define(MAX_TAG_NAME_SIZE, 3072).
 -define(MAX_TAG_VALUE_SIZE, 3072).
+
+%% The hashing algorithm used to calculate wallet addresses.
+-define(HASH_ALG, sha256).
+
+-define(RSA_SIGN_ALG, rsa).
+-define(RSA_PRIV_KEY_SZ, 4096).
+
+-define(ECDSA_SIGN_ALG, ecdsa).
+-define(ECDSA_TYPE_BYTE, <<2>>).
+
+-define(EDDSA_SIGN_ALG, eddsa).
+-define(EDDSA_TYPE_BYTE, <<3>>).
+
+%% The default key type used by transactions that do not specify a signature type.
+-define(DEFAULT_KEY_TYPE, {?RSA_SIGN_ALG, 65537}).
+
+%% Winstons per AR.
+-define(WINSTON_PER_AR, 1000000000000).
+
+%% A macro to convert AR into Winstons.
+-define(AR(AR), (?WINSTON_PER_AR * AR)).
+
 %% @doc A transaction.
 -record(tx, {
     %% 1 or 2 or ans104.
@@ -18,6 +48,8 @@
     last_tx = <<>>,
     %% The public key the transaction is signed with.
     owner =	?DEFAULT_OWNER,
+	%% The owner address. Used as a cache to avoid recomputing it, not serialized.
+	owner_address = not_set,
     %% A list of arbitrary key-value pairs. Keys and values are binaries.
     tags = [],
     %% The address of the recipient, if any. The SHA2-256 hash of the public key.
@@ -61,20 +93,7 @@
 
     %% The type of signature this transaction was signed with. A system field,
     %% not used by the protocol yet.
-    signature_type = {rsa, 65537}
+    signature_type = ?DEFAULT_KEY_TYPE
 }).
 
-%% The hashing algorithm used to calculate wallet addresses.
--define(HASH_ALG, sha256).
 
--define(RSA_SIGN_ALG, rsa).
--define(RSA_PRIV_KEY_SZ, 4096).
-
--define(ECDSA_SIGN_ALG, ecdsa).
--define(ECDSA_TYPE_BYTE, <<2>>).
-
--define(EDDSA_SIGN_ALG, eddsa).
--define(EDDSA_TYPE_BYTE, <<3>>).
-
-%% The default key type used by transactions that do not specify a signature type.
--define(DEFAULT_KEY_TYPE, {?RSA_SIGN_ALG, 65537}).
